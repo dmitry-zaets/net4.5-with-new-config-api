d99cf267507975c733c799ce4e294ed2f42a52b0
Dmitry Zaets <dmitry.zaets@gmail.com>
2017-05-08 01:38:49 +0200

    Use Configuration Option classes in Controller

--- src/Net45WithNetCoreConfigApi/Controllers/HomeController.cs
+++ src/Net45WithNetCoreConfigApi/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Options;
+using Net45WithNetCoreConfigApi.Configurations.Feed;
+using Net45WithNetCoreConfigApi.Configurations.Mailing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,20 @@ namespace Net45WithNetCoreConfigApi.Controllers
 {
     public class HomeController : Controller
     {
+        private MailingOptions _mailingOptions;
+        private FeedOptions _feedOptions;
+
+        public HomeController(IOptionsSnapshot<MailingOptions> mailingOptions, IOptions<FeedOptions> feedOptions)
+        {
+            _mailingOptions = mailingOptions.Value;
+            _feedOptions = feedOptions.Value;
+        }
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-
+            ViewBag.BatchDelay = _mailingOptions.BatchDelay;
+            ViewBag.BatchSize = _mailingOptions.BatchSize;
+            ViewBag.FeedEndpoints = string.Join(",", _feedOptions.Endpoints.Select(_=>_.Name));
             return View();
         }
     }
