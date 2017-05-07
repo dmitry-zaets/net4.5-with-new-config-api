1b1a330cc74860db27b2717dad5407ec38dff822
Dmitry Zaets <dmitry.zaets@gmail.com>
2017-05-07 23:43:00 +0200

    Add clean demo app

---
+++ src/Net45WithNetCoreConfigApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Net45WithNetCoreConfigApi.WebApiApplication" Language="C#" %>
---
+++ src/Net45WithNetCoreConfigApi/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Net45WithNetCoreConfigApi.Startup))]
+
+namespace Net45WithNetCoreConfigApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+    }
+}
